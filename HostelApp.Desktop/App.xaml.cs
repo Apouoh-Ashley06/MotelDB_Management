3a2d45054c3ad5c6dc89497f2e6cb56bc246ba56
Apouoh-Ashley06 <apasham06@gmail.com>
2024-11-06 09:20:10 +0300

    Project for the motel AYUK

---
+++ HostelApp.Desktop/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HostelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ServiceProvider ServiceProvider;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<CheckInForm>();
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+            services.AddTransient<ISqliteDataAccess, SqliteDataAccess>();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+
+            services.AddSingleton(config);
+            string dbChoice = config.GetValue<string>("DatabaseChoice").ToLower();
+            if (dbChoice == "sql")
+            {
+                services.AddTransient<IDatabaseData, SqlData>();
+            }
+            else if (dbChoice == "sqlite")
+            {
+                services.AddTransient<IDatabaseData, SqliteData>();
+            }
+            else
+            {
+                services.AddTransient<IDatabaseData, SqlData>();
+            }
+
+            ServiceProvider = services.BuildServiceProvider();
+            var mainWindow = ServiceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
+    }
+}
---
+++ HostelDb/dbo/Stored Procedures/spBookings_Insert.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Insert]
+	@roomId int,
+	@guestId int,
+	@startDate date,
+	@endDate date,
+	@totalcost money
+
+AS
+	begin
+		set nocount on;
+
+		insert into dbo.Bookings(RoomId, GuestId, StartDate, EndDate, TotalCost)
+		values (@roomId, @guestId, @startDate, @endDate, @totalcost);
+
+	end
+RETURN 0
